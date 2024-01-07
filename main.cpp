71f8981587e870fd6de4216e08e58e7f43c8c463
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2024-01-06 23:56:06 -0600

    added the algorithm function

--- main.cpp
+++ main.cpp
@@ -17,3 +17,28 @@ struct ThreadData {
   pthread_cond_t *turn_cond;         // Pointer to the condition variable for turn-based synchronization
 };
 
+// Function to calculate incremental entropy
+double calc_incremental_entropy(std::map<char, int> &freq, int &currFreq, double &currH, char selectedTask, int extraFreq, int &NFreq) {
+  double H, newTerm, currentTerm;
+
+  // Update the total frequency
+  NFreq = currFreq + extraFreq;
+
+  // Calculate the entropy
+  if (NFreq == extraFreq) {
+    H = 0;
+  } else {
+    currentTerm = freq[selectedTask] == 0 ? 0 : freq[selectedTask]*log2(freq[selectedTask]);
+    newTerm = (freq[selectedTask] + extraFreq) * log2(freq[selectedTask] + extraFreq);
+    NFreq = static_cast<double>(NFreq);
+    H = log2(NFreq) - ((log2(currFreq) - currH) * (currFreq)-currentTerm + newTerm) / NFreq;
+  }
+
+  // Update the current frequency and entropy
+  currFreq = NFreq;
+  currH = H;
+
+  return H;
+};
+
+
